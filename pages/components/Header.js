d5d9d3cd81cd00f928affa9ee4e48fa1b7be0bf2
Hamad <HamadNabizada@gmail.com>
2022-05-18 06:23:11 -0700

    created initial files. added header and styling modules

---
+++ pages/components/Header.js
@@ -0,0 +1,9 @@
+import styles from '../../styles/Header.module.css'
+
+export default function Header(){
+    return(
+        <>
+            <h1 className={styles.wrapper}>Text AI</h1>
+        </>
+    )
+}
